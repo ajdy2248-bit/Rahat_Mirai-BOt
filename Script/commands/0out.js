3836d8ee00a87a9d8c5db2f51b70650ff9107293
ajdy2248-bit <ajdy2248@gmail.com>
2026-01-22 06:25:29 +0100

    Update 0out.js

--- Script/commands/0out.js
+++ Script/commands/0out.js
@@ -1,15 +1,18 @@
-module.exports.config = {
+Entermodule.exports.config = {
     name: "out",
     version: "1.0.0",
     hasPermssion: 2,
     credits: "🔰𝐑𝐀𝐇𝐀𝐓 𝐈𝐒𝐋𝐀𝐌🔰",
-    description: "",
-    commandCategory: "Admin",
+    description: "إخراج البوت من المجموعة",
+    commandCategory: "المدراء",
     usages: "out [id]",
     cooldowns: 10,
 };
 
 module.exports.run = async function({ api, event, args }) {
-        if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
-}
+    if (!args[0]) 
+        return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+
+    if (!isNaN(args[0])) 
+        return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
+};
