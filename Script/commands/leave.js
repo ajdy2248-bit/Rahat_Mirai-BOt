ba76e4abc26b3a061632e53331c732c3ef3c8aba
ajdy2248-bit <ajdy2248@gmail.com>
2026-01-22 06:56:41 +0100

    Update leave.js

--- Script/commands/leave.js
+++ Script/commands/leave.js
@@ -1,17 +1,17 @@
 module.exports.config = {
     name: "leave",
     version: "1.0.0",
-    hasPermssion: 2,  // শুধুমাত্র admin-level ইউজার চালাতে পারবে
-    credits: "rX Abdullah",
-    description: "Make the bot leave the group",
-    commandCategory: "System", // 🔑 এইটা না থাকলে load হবে না
+    hasPermssion: 2,  // فقط المستخدمون بمستوى ادمن يمكنهم التشغيل
+    credits: "rX عبدالله",
+    description: "اجعل البوت يغادر المجموعة",
+    commandCategory: "النظام", // 🔑 بدون هذا لن يعمل
     usages: "leave",
     cooldowns: 5
 };
 
 module.exports.run = async ({ api, event }) => {
     const { threadID } = event;
-    api.sendMessage("👋 Bot is leaving this group!", threadID, () => {
+    api.sendMessage("👋 البوت سيغادر هذه المجموعة!", threadID, () => {
         api.removeUserFromGroup(api.getCurrentUserID(), threadID);
     });
 };
