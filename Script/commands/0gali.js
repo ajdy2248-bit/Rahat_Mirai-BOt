1ff0111415369afe4297b6107e85c796b6718d18
ajdy2248-bit <ajdy2248@gmail.com>
2026-01-22 06:20:36 +0100

    Update 0gali.js

--- Script/commands/0gali.js
+++ Script/commands/0gali.js
@@ -1,26 +1,35 @@
 const fs = require("fs");
+
 module.exports.config = {
 	name: "gali",
-    version: "1.0.1",
+	version: "1.0.1",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", //Modify by rX
-	description: "Don't Change Credits",
-	commandCategory: "no prefix",
+	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", // Modify by rX
+	description: "لا تقم بتغيير الحقوق",
+	commandCategory: "بدون بادئة",
 	usages: "mgi",
-    cooldowns: 5, 
+	cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Mgi")==0 || event.body.indexOf("Khanki")==0 || event.body.indexOf("Mg")==0 || event.body.indexOf("Magi")==0) {
-		var msg = {
-				body: "aisob kintu vlo lge na",
-				attachment: fs.createReadStream(__dirname + `/noprefix/nupure.mp3`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("😡", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
+	if (
+		event.body.indexOf("Mgi") == 0 ||
+		event.body.indexOf("Khanki") == 0 ||
+		event.body.indexOf("Mg") == 0 ||
+		event.body.indexOf("Magi") == 0
+	) {
+		var msg = {
+			body: "هذا الكلام غير مقبول",
+			attachment: fs.createReadStream(__dirname + `/noprefix/nupure.mp3`)
+		};
+
+		api.sendMessage(msg, threadID, messageID);
+		api.setMessageReaction("😡", event.messageID, (err) => {}, true);
 	}
+};
+
+module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+};
