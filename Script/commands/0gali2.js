7e72006cc6e628e7d64adc9419d0ab5299384753
ajdy2248-bit <ajdy2248@gmail.com>
2026-01-22 06:23:10 +0100

    Update 0gali2.js

--- Script/commands/0gali2.js
+++ Script/commands/0gali2.js
@@ -1,26 +1,35 @@
-const fs = require("fs");
+Enterconst fs = require("fs");
+
 module.exports.config = {
 	name: "suar",
-    version: "1.0.1",
+	version: "1.0.1",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", //Modify by rX
-	description: "jakiya's gali",
-	commandCategory: "no prefix",
+	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", // Modify by rX
+	description: "شتيمة جاكيا",
+	commandCategory: "بدون بادئة",
 	usages: "suar",
-    cooldowns: 5, 
+	cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("suar")==0 || event.body.indexOf("Suar")==0 || event.body.indexOf("sor kuttar bacca")==0 || event.body.indexOf("সুয়ার")==0) {
-		var msg = {
-				body: "jaki 🎀",
-				attachment: fs.createReadStream(__dirname + `/noprefix/suar.mp3`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("😡", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-  }
+	if (
+		event.body.indexOf("suar") == 0 ||
+		event.body.indexOf("Suar") == 0 ||
+		event.body.indexOf("sor kuttar bacca") == 0 ||
+		event.body.indexOf("সুয়ার") == 0
+	) {
+		var msg = {
+			body: "جاكيا 🎀",
+			attachment: fs.createReadStream(__dirname + `/noprefix/suar.mp3`)
+		};
+
+		api.sendMessage(msg, threadID, messageID);
+		api.setMessageReaction("😡", event.messageID, (err) => {}, true);
+	}
+};
+
+module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+};
